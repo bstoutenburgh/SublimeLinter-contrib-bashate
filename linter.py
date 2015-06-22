bb0bc3f907537d7d0cf366e3b986beeebe061c18
Ben Stoutenburgh <maristgeek@gmail.com>
2015-06-21 21:24:52 -0400

    update regex to flag warnings, addressed #4

--- linter.py
+++ linter.py
@@ -21,8 +21,9 @@ class Bashate(Linter):
     cmd = 'bashate'
     comment_re = r'\s*#'
     regex = (
-        r'^(\[[EW]\])? (?P<error>[E])\d+: (?P<message>.+): '
-        r'\'(?P<near>.+)\'\n - (?P<file>.+) : L(?P<line>\d+)'
+        r'^\[(?:(?P<error>E)|(?P<warning>W))\] E\d+: '
+        r'(?P<message>.+): \'(?P<near>.+)\'\n - '
+        r'(?P<file>.+) : L(?P<line>\d+)'
     )
     multiline = True
     defaults = {
